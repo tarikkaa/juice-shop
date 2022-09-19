c659fbd5749e2fea73bbef7735d57d96c4973d15
taras.work <testfromtaras@gmail.com>
2022-09-19 23:52:20 +0300

    Allure reporter configured, all tests passed

--- pages/facebookMainPage.js
+++ pages/facebookMainPage.js
@@ -3,7 +3,7 @@ const ViewElement = require("../elements/view.element");
 const BasePage = require("./basePage");
 
 class FacebookMainPage extends BasePage{
-    baseElement() {return new ViewElement($('[1mask="url(#jsc_c_p)"'), "Facebook main page base element")};
+    baseElement() {return new ViewElement($('[mask="url(#jsc_c_p)"'), "Facebook main page base element")};
     get accountButton() {return new WebButton($('[mask="url(#jsc_c_p)"'), "Facebook main page account button")};
 
 }
